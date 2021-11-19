d10f94afba5f2d709e55042d2e2f06b959fb9609
dewo952 <67516219+dewo952@users.noreply.github.com>
2021-11-19 21:09:37 +0530

    Update CovidDeaths Exploration.sql

--- CovidDeaths Exploration.sql
+++ CovidDeaths Exploration.sql
@@ -143,30 +143,16 @@ Queries used for Tableau Project
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From [portfolio project]..CovidDeaths
---Where location like '%states%'
+--Where location like '%India%'
 where continent is not null 
 --Group By date
 order by 1,2
 
---The Second includes "International"  Location
-
-
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
-
-
 -- 2. 
-
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From [portfolio project]..CovidDeaths
---Where location like '%states%'
+--Where location like '%India%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
 Group by location
@@ -177,7 +163,7 @@ order by TotalDeathCount desc
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From [portfolio project]..CovidDeaths
---Where location like '%states%'
+--Where location like '%India%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -187,6 +173,6 @@ order by PercentPopulationInfected desc
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From [portfolio project]..CovidDeaths
---Where location like '%states%'
+--Where location like '%India%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
